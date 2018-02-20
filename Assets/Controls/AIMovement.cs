5f961dc62e0f6c44cd7a86253213524ea18029db
DarkXieon <andrewkausy@gmail.com>
2018-02-20 18:30:48 -0500

    movement

---
+++ Assets/Controls/AIMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Contracts;
+
+public class AIMovement : MoveableObject
+{
+    //The AI logic would go here
+    protected override Vector2 GetMovement()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Controls/CharacterMovement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Controls
+{
+    public class CharacterMovement : MoveableObject
+    {
+        protected override Vector2 GetMovement()
+        {
+            //return the Horisontal move axis as a Vector2
+            return new Vector2(Input.GetAxis("Horizontal"), 0f);
+        }
+    }
+}
