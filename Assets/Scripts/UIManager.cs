c62b6dbb4ec669894266cdc93e4b4c28892a35a2
Kyle O'Mara <kylepomara@gmail.com>
2018-02-22 15:31:27 -0500

    Added Main Menu and Pause Menu functionality.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    GameObject[] pauseObjects;
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        hidePaused();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //uses the p button to pause and unpause the game
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                showPaused();
+            }
+            else if (Time.timeScale == 0)
+            {
+                Debug.Log("high");
+                Time.timeScale = 1;
+                hidePaused();
+            }
+        }
+    }
+
+
+    //Reloads the Level - Restart Button
+    public void Reload() 
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    //controls the pausing of the scene - Resume Button
+    public void pauseControl()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            showPaused();
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            hidePaused();
+        }
+    }
+
+    //shows objects with ShowOnPause tag
+    public void showPaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    //hides objects with ShowOnPause tag
+    public void hidePaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    //loads inputed level - Main Menu button
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(0);
+        //Application.LoadLevel(level);
+    }
+}
---
+++ Assets/test_onclick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test_onclick : MonoBehaviour {
+    public Button btn;
+	// Use this for initialization
+	void Start () {
+        this.gameObject.GetComponent<Button>().onClick.AddListener(TaskOnClick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TaskOnClick() {
+        Debug.LogWarning("You have clicked the button");
+    }
+}
