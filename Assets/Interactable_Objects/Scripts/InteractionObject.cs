bd1d18a581afc798722e75f15b8ec54cf1bd1773
Tim Sherry <timsherry97@gmail.com>
2018-02-27 17:37:55 -0500

    Made an Interactable "Lever"
    If you go to the capsule (named lever) at the right of the play field, and press e, it displays "Do something..." in the console. If there was going to be something done, It would be done. This is done using the InteractionObject script, which can be attached to any GameObject with a  Circle Collider 2D, and that GameObject will do something when E is pressed.

---
+++ Assets/Interactable_Objects/Scripts/InteractionObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour {
+
+    public void DoInteraction()
+    {
+        // Do Something...
+        Debug.Log("Does Something...");
+    }
+}
