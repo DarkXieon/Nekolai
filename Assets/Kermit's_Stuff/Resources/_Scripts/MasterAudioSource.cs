70936d35b9448cf8912b5a2cdba394789c64c167
DarkXieon <andrewkausy@gmail.com>
2018-03-01 14:02:38 -0500

    jkflds

---
+++ Assets/Kermit's_Stuff/Resources/_Scripts/MasterAudioSource.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterAudioSource : MonoBehaviour
+{
+    // Declare the variables
+
+    public static MasterAudioSource mas; // a singleton that accesses all audio files
+
+    private void Awake()
+    {
+
+        // Singleton logic
+        if(mas == null)
+        {
+            mas = this;
+            DontDestroyOnLoad(mas);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // pauses all audio in game
+    public void PauseAllAudio()
+    {
+        foreach(AudioSource audioSource in mas.GetComponentsInChildren<AudioSource>())
+        {
+            if (audioSource.isPlaying)
+            {
+                // Debug.Log(audioSource.gameObject.name + " | " + Time.time);
+                audioSource.Pause();
+                audioSource.gameObject.AddComponent<Stat>(); 
+                // ^^ Mark audio paused by this function with an unusual component to make unpausing them easier
+            }
+        }
+    }
+
+    // unpauses all audio in game, 
+    public void UnPauseAllAudio()
+    {
+        foreach (AudioSource audioSource in mas.GetComponentsInChildren<AudioSource>())
+        {
+            if (audioSource.GetComponent<Stat>() != null)
+            {
+                // Debug.Log(audioSource.gameObject.name + " | " + Time.time);
+                audioSource.UnPause();
+                Destroy(audioSource.GetComponent<Stat>()); // Get rid of the unusual component from PauseAllAudio
+            }
+        }
+    }
+
+    // stops all audio clips in the game
+    public void StopAllAudio()
+    {
+        foreach (AudioSource audioSource in mas.GetComponentsInChildren<AudioSource>())
+        {
+            // Debug.Log(audioSource.gameObject.name + " | " + Time.time);
+            audioSource.Stop();
+           // audioSource.
+                
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    GameObject[] pauseObjects;
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        hidePaused();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //uses the p button to pause and unpause the game
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            pauseControl(); // How come you're not using this function instead? Left by: Kermit
+            /*
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                showPaused();
+            }
+            else if (Time.timeScale == 0)
+            {
+                Debug.Log("high");
+                Time.timeScale = 1;
+                hidePaused();
+            }*/
+        }
+    }
+
+
+    //Reloads the Level - Restart Button
+    public void Reload() 
+    {
+        MasterAudioSource.mas.StopAllAudio(); // ALWAYS stop all audio before changing scenes
+
+        // Play the appropiate audio depeneding on the scene name
+
+        if(SceneManager.GetActiveScene().name == "Level 1")
+        {
+            MasterAudioSource.mas.transform.Find("Level_1").GetComponent<AudioSource>().Play();
+        }
+
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    //controls the pausing of the scene - Resume Button
+    public void pauseControl()
+    {
+        if (Time.timeScale == 1)
+        {
+            // Pauses the game -- Comment left by: Kermit
+            MasterAudioSource.mas.PauseAllAudio();
+            Time.timeScale = 0;
+            showPaused();
+            
+        }
+        else if (Time.timeScale == 0)
+        {
+            // Unpauses the game -- Comment left by: Kermit
+            Time.timeScale = 1;
+            hidePaused();
+            MasterAudioSource.mas.UnPauseAllAudio();
+
+        }
+    }
+
+    //shows objects with ShowOnPause tag
+    public void showPaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    //hides objects with ShowOnPause tag
+    public void hidePaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    //loads inputed level - Main Menu button
+    public void LoadLevel(string level)
+    {
+        MasterAudioSource.mas.StopAllAudio();
+        SceneManager.LoadScene(0);
+        //Application.LoadLevel(level);
+    }
+}
