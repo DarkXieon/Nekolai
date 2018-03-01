804470b9aa0640a7543bb42adf082e240adf243f
DarkXieon <andrewkausy@gmail.com>
2018-03-01 14:22:05 -0500

    test

--- Assets/Kermit's_Stuff/Resources/_Scripts/Stat_Speed.cs
+++ Assets/Kermit's_Stuff/Resources/_Scripts/Stat_Speed.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class Stat_Speed : Stat
 {
     public void Start()
-    {
-<<<<<<< HEAD
+    { 
         this.SetBaseValue(15.0f);
-=======
+
         this.SetBaseValue(5.0f);
->>>>>>> TimBranch
+
         base.Start();
     }
 }
