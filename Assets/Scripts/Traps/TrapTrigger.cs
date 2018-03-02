0d15bee5d46f4bb062d97c9bfeaff1e0e960c1e3
DarkXieon <andrewkausy@gmail.com>
2018-03-01 23:38:24 -0500

    Organized

--- Assets/Scripts/Traps/TrapTrigger.cs
+++ Assets/Scripts/Traps/TrapTrigger.cs
@@ -4,33 +4,23 @@ using System.Collections;
 public class TrapTrigger : MonoBehaviour, ITrapTrigger
 {
     [SerializeField]
-    private GameObject _trapOn;
-    
-    private ITrap _trap;
+    private Trap _trap;
 
     // Use this for initialization
-    void Start()
+    private void Start()
     {
-        _trap = _trapOn.GetComponent<ITrap>();
-
         Debug.Assert(_trap != null, "There is no trap on the provided object");
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void OnTriggerEnter2D(Collider2D other)
     {
-
+        this.TriggerTrap();
+        
+        Destroy(this.gameObject);
     }
     
     public void TriggerTrap()
     {
         _trap.Activate();
-    }
-    
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        this.TriggerTrap();
-
-        Destroy(this);
     }
 }
