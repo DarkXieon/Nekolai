18822ff239cb4991a49a3d0f00566503f2c78a58
DarkXieon <andrewkausy@gmail.com>
2018-03-01 19:46:13 -0500

    Organized

---
+++ Assets/Scripts/Traps/FloorDestroyer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+public class FloorDestroyer : MonoBehaviour, ITrap
+{
+    [SerializeField]
+    private GameObject _toDestroy;
+
+    public void Activate()
+    {
+        var rigidbody2D = this.GetComponent<Rigidbody2D>();
+
+        rigidbody2D.isKinematic = false;
+
+        Debug.Log("Activated");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Entered");
+
+        Debug.Log(other.gameObject.name);
+
+        if(other.gameObject.Equals(_toDestroy))
+        {
+            Destroy(this._toDestroy);
+            Destroy(this.gameObject);
+        }
+        /*
+        other.gameObject.SetActive(false);
+
+        Destroy(other.gameObject);
+        Destroy(this.gameObject);
+        */
+    }
+}
---
+++ Assets/Scripts/Traps/TrapTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapTrigger : MonoBehaviour, ITrapTrigger
+{
+    [SerializeField]
+    private GameObject _trapOn;
+    
+    private ITrap _trap;
+
+    // Use this for initialization
+    void Start()
+    {
+        _trap = _trapOn.GetComponent<ITrap>();
+
+        Debug.Assert(_trap != null, "There is no trap on the provided object");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void TriggerTrap()
+    {
+        _trap.Activate();
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        this.TriggerTrap();
+
+        Destroy(this);
+    }
+}
