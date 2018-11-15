397de0a8f93c716169d68de0120daf26cdaffda3
DarkXieon <andrewkausy@gmail.com>
2018-11-15 15:00:14 -0500

    Did some updating/organizing

--- Assets/Resources/Scripts/UI/Grid/PathfindingFallMarker.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public class PathfindingFallMarker : PathfindingMarker
-{
-    protected override Color gizmoColor { get { return Color.red; } }
-}
-
--- Assets/Resources/Scripts/UI/Grid/PathfindingJumpMarker.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class PathfindingJumpMarker : PathfindingMarker
-{
-    public HorizontalDirection JumpDirection;
-
-    protected override Color gizmoColor { get { return Color.yellow; } }
-}
